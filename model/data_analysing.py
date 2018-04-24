adac2ba581b35b08c53cebf5f4f9e2d988ff40b4
Mohsen Naghipourfar <mn7697np@gmail.com>
2018-04-25 01:30:11 +0430

    Fix bug in subplots

--- model/data_analysing.py
+++ model/data_analysing.py
@@ -15,6 +15,7 @@ diseases = labels[0].value_counts().index
 for i in range(x_train.shape[1]):
     data_to_plot = pd.concat([x_train[i], labels], axis=1)
     data_to_plot.columns = ['feature', 'label']
+    f, ax = plt.subplots(figsize=(20, 20))
     plt.xticks(rotation=90)
     sns.boxplot(x='label', y='feature', data=data_to_plot)
     plt.ylabel('feature {0}'.format(i))
