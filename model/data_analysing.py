78b9d6f9355b7607190c8cd3e026af2ba40186ed
Mohsen Naghipourfar <mn7697np@gmail.com>
2018-04-25 01:24:54 +0430

    add python analysis file

--- model/data_analysing.py
+++ model/data_analysing.py
@@ -1,9 +1,6 @@
-import numpy as np
 import pandas as pd
 import seaborn as sns
-from sklearn.feature_selection import mutual_info_classif
 
-import tensorflow as tf
 import matplotlib.pyplot as plt
 
 """
