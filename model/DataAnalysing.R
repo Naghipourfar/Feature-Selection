8bd26877c91553ad135799771b0d6a1faacd5bf2
Mohsen Naghipourfar <mn7697np@gmail.com>
2018-10-16 22:53:20 +0330

    rename "Model" folder to "Code"

--- model/DataAnalysing.R
+++
@@ -1,5 +0,0 @@
-data <- read_csv('/Users/Future/Desktop/Spring 2018/Projects/Bioinformatics/Feature Selection/model/Results/CAE/Damavand/encoded_results_0.01_128.csv', col_names = c("MSE"))
-data$MSE <- as.numeric(data$MSE)
-hist(x = data$MSE, breaks = 30, col = 'red', xlab = 'MSE Loss', main = 'Distribution Plot (stddev = 0.01)')
-plot(density(data$MSE))
-
--- model/test_gpu.sh
+++
@@ -1,2 +0,0 @@
-#!/usr/bin/env bash
-nvidia-smi | awk '/GeForce/ {getline;print $13}' | sed 's/%//g'
